ad4cc004943eb0bd2e6e94d3835a04f6cc6a9f9c
zhangmaosen <xiaosen.zhang@gmail.com>
2023-07-21 17:20:50 +0800

    add model names

--- apps/models_map.py
+++ apps/models_map.py
@@ -6,7 +6,7 @@ model_names_map = {'mpnet':'sentence-transformers/multi-qa-mpnet-base-dot-v1', #
                'all_mpnet':'sentence-transformers/all-mpnet-base-v2', # 5
                'shibing_paraphrase':'shibing624/text2vec-base-chinese-paraphrase', # 6
                'luotuo':'silk-road/luotuo-bert-medium',
-               'qa_mpnet':'sentence-transformers/multi-qa-mpnet-base-cos-v1', # 8
+               'qa_mpnet':'sentence-transformers/paraphrase-multilingual-mpnet-base-v2', # 8
                'my-184':'C:\\Users\\zhang\\dev\\sentence-transformers\\output\\training_paraphrases_hfl-chinese-lert-large-2023-07-18_21-11-13\\184',
     }
 
